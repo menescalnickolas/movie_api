dd82a79012462f9d9e93cab897b0d252d5a1be87
menescalnickolas <menescalnickolas@gmail.com>
2024-06-11 21:53:27 +0200

    Added Create New User Request

--- server.js
+++ server.js
@@ -20,6 +20,35 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
+//Create New User (CREATE)
+app.post('/users', async (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    .then((user) => {
+      if (user) {
+      //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
 
 // Update User Info (UPDATE)
 app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
