61a24c82639060b5cf09b6305d9e7aecfac7ef8d
menescalnickolas <menescalnickolas@gmail.com>
2024-06-12 11:15:48 +0200

    Added validation to Create New User

--- server.js
+++ server.js
@@ -23,7 +23,19 @@ const passport = require('passport');
 require('./passport');
 
 //Create New User (CREATE)
-app.post('/users', async (req, res) => {
+app.post('/users', [
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], async (req, res) => {
+
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
   let hashedPassword = Users.hashPassword(req.body.Password);
   await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
     .then((user) => {
