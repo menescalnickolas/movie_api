3fe9bfde1421f2e9fce026632a5c8c3c2ef98194
menescalnickolas <menescalnickolas@gmail.com>
2024-06-13 14:05:10 +0200

    Required cors

--- server.js
+++ server.js
@@ -5,9 +5,23 @@ const express = require('express'),
   mongoose = require('mongoose'),
   Models = require('./models.js'),
   Movies = Models.Movie,
-  Users = Models.User;
+  Users = Models.User,
+  cors = require('cors');
 
   const { check, validationResult } = require('express-validator');
+
+  let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // mongoose.connect('mongodb://localhost:27017/TestFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
