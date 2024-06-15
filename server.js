524e1cd85c3ef31b0ba4aa79f84792e66d7e4791
menescalnickolas <menescalnickolas@gmail.com>
2024-06-15 11:37:19 +0200

    Removed quotes from mongoose.connect

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const { check, validationResult } = require('express-validator');
 
 // mongoose.connect('mongodb://localhost:27017/TestFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect("process.env.CONNECTION_URI", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
