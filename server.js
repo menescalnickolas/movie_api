cfb68d009dc5ee74a632c489f57bd00e29ffce8e
menescalnickolas <menescalnickolas@gmail.com>
2024-06-15 10:56:23 +0200

    Updated app.listen

--- server.js
+++ server.js
@@ -214,6 +214,6 @@ app.get('/movies/directors/:directorName', passport.authenticate('jwt', { sessio
 
 
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0', () => {
-  console.log('Listening on Port ' + port);
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
